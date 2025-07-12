cfe6d7a9f00a41516562b8e146e8783d9bdcc25d
Edwin Krasnokutsky <Edwinkras29@gmail.com>
2025-07-11 23:47:27 -0400

    Second query

--- steam_select.sql
+++ steam_select.sql
@@ -6,3 +6,11 @@ from store
 inner join steam.games g on g.gid = store.gid
 where price >= 50
 order by price asc;
+
+-- 2. Find all users that been on steam for at least 4.5 years
+
+select *
+from user_account
+where date_created < current_date - interval '4.5 years';
+
+-- Sorry if I couldn't add more before the time limit
