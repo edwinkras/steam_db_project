ab83aadc30281f5810b9e35fd67111d897ff0f45
Edwin Krasnokutsky <Edwinkras29@gmail.com>
2025-07-11 20:54:05 -0400

    Adding Values to the tables User Account and Friends
    Adding values as you can see below to User Account and Friends

---
+++ steam_insert.sql
@@ -0,0 +1,19 @@
+set search_path to steam;
+
+INSERT INTO user_account(username, date_created)
+VALUES ('cloudStrife7', '2004-11-23'),
+       ('c0l0rs', '1997-06-14'),
+       ('Donko', '1998-03-09'),
+       ('LexxLittle', '2002-11-18');
+
+INSERT INTO friends(username, friends_with, friends_since)
+VALUES ('Frostbyte_', '3', '2022-06-03'),
+       ('novaa', '1', '2019-11-15'),
+       ('Russianbro', '3', '2020-04-02'),
+       ('JaySu', '4', '2018-12-10'),
+       ('goldd', '2', '2023-06-14'),
+       ('dubzyR6S', '4', '2022-11-03'),
+       ('klein7', '3', '2024-03-15');
+
+-- INSERT INTO games(name, uid, fid) VALUES
+
