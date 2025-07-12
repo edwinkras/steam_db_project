16b96184de86ab8cd5c378a939723067bd2d7376
Edwin Krasnokutsky <Edwinkras29@gmail.com>
2025-07-11 21:14:58 -0400

    adding games to the game tables
    Adding a couple of games that are similar while still trying to have different genres put

--- steam_insert.sql
+++ steam_insert.sql
@@ -15,5 +15,18 @@ VALUES ('Frostbyte_', '3', '2022-06-03'),
        ('dubzyR6S', '4', '2022-11-03'),
        ('klein7', '3', '2024-03-15');
 
--- INSERT INTO games(name, uid, fid) VALUES
+INSERT INTO games(name)
+VALUES ('Cyberpunk 2077'), -- open world
+       ('DOOM Eternal'), -- FPS
+       ('Call Of Duty: Black Ops 2'), -- FPS
+       ('Resident Evil 4'), -- Horror
+       ('Final Fantasy 7 Rebirth'), -- JRPG
+       ('Red Dead Redemption 2'), -- open world
+       ('Tekken 8'), -- Fighting game
+       ('Mortal Kombat 1'), -- Fighting game
+       ('Chrono Trigger'), -- JRPG
+       ('The Last Of Us Part 1'), -- Story-game
+       ('Dead Space'), -- Horror
+       ('Silent Hill 2'); -- Horror
+
 
